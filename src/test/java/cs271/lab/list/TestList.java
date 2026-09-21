0ef1880c2b48e83e4137dd751a832e2eb98d787c
Meg Knoernschild <megan@seattleweb.com>
2026-09-21 11:41:03 -0500

    Fix testAddMultiple2() assertions

--- src/test/java/cs271/lab/list/TestList.java
+++ src/test/java/cs271/lab/list/TestList.java
@@ -64,7 +64,7 @@ public class TestList {
     list.add(77);
     list.add(77);
     list.add(77);
-    // TODO Write assertions using list.contains(77) below
+    // TODO Write assertions using list.contains(77) below: completed
     assertEquals(3, list.size());
     assertEquals(0, list.indexOf(77));
     assertEquals(77, list.get(1).intValue());
@@ -80,13 +80,13 @@ public class TestList {
     list.add(55);
     list.add(77);
     list.add(66);
-    // TODO fix the expected values in the assertions below
-    assertEquals(0, list.size());
-    assertEquals(0, list.indexOf(77));
-    assertEquals(0, list.lastIndexOf(77));
-    assertEquals(0, list.get(2).intValue());
-    assertEquals(0, list.get(3).intValue());
-    assertEquals(List.of(33, 77, 44), list);
+    // TODO fix the expected values in the assertions below: completed
+    assertEquals(7, list.size());
+    assertEquals(1, list.indexOf(77));
+    assertEquals(5, list.lastIndexOf(77));
+    assertEquals(44, list.get(2).intValue());
+    assertEquals(77, list.get(3).intValue());
+    assertEquals(List.of(33, 77, 44, 77, 55, 77, 66), list);
   }
 
   @Test
